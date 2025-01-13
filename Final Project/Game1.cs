fb3e56f63865f0e35ac20ea3b6574bcecd8ed4e3
Raine DeVries <362571390@GoTVDSB.ca>
2025-01-13 08:27:44 -0500

    Working

--- Final Project/Game1.cs
+++ Final Project/Game1.cs
@@ -6,27 +6,47 @@ namespace Final_Project
 {
     public class Game1 : Game
     {
+        enum Screen
+        {
+            //Intro,
+            Game
+        }
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
-
+        Screen screen;
+        KeyboardState keyboardState;
+        Texture2D track;
+        Rectangle trackRect1, trackRect2;
+        int scrollSpeed = 1;
+        int maxSpeed = 20;
+        int acceleration = 1;
+        float seconds;
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
-            IsMouseVisible = true;
+            
+
+            
         }
 
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
-
+            //screen = Screen.Intro;
+            keyboardState = Keyboard.GetState();
+            
+            int screenWidth = GraphicsDevice.Viewport.Width;
+            int trackHeight = 1000;
+            trackRect1 = new Rectangle(0, 0, screenWidth, trackHeight);
+            trackRect2 = new Rectangle(0, -trackHeight, screenWidth, trackHeight);
             base.Initialize();
         }
 
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
-
+            track = Content.Load<Texture2D>("Track");
             // TODO: use this.Content to load your game content here
         }
 
@@ -34,7 +54,27 @@ namespace Final_Project
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
+            //if (keyboardState.GetPressedKeyCount() > 0)
+            //screen = Screen.Game;
+            seconds += (float)gameTime.ElapsedGameTime.TotalSeconds;
+            scrollSpeed += (acceleration * (int)seconds);
+            if (scrollSpeed > maxSpeed)
+                scrollSpeed = maxSpeed; //max
+            
+            trackRect1.Y += scrollSpeed;
+            trackRect2.Y += scrollSpeed;
 
+            int screenHeight = GraphicsDevice.Viewport.Height;
+
+            if (trackRect1.Y >= screenHeight)
+                trackRect1.Y = trackRect2.Y - trackRect1.Height;
+            if (trackRect2.Y >= screenHeight)
+                trackRect2.Y = trackRect1.Y - trackRect2.Height;
+
+            
+            
+                
+            
             // TODO: Add your update logic here
 
             base.Update(gameTime);
@@ -44,7 +84,18 @@ namespace Final_Project
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            // TODO: Add your drawing code here
+            //if (screen == Screen.Intro)
+            
+
+           
+            if (screen == Screen.Game)
+            {
+                _spriteBatch.Begin();
+                _spriteBatch.Draw(track, trackRect1, Color.White);
+                _spriteBatch.Draw(track, trackRect2, Color.White);
+                _spriteBatch.End();
+            }
+
 
             base.Draw(gameTime);
         }
